17fbdb6bcfcde1b939adc5d8120f2d928049a5b4
John Vilk <jvilk@cs.umass.edu>
2015-10-05 17:22:51 -0400

    Adding the ability to *ignore* missing mappings.
    Adding a test that exercises this option from a real-world program, and bumps the version
    number to a new version to indicate the new API functionality.

--- Library/SourceMapMerger.js
+++ Library/SourceMapMerger.js
@@ -1,12 +1,20 @@
 var fs = require("fs");
 var SourceMap = require("source-map");
 
-var findOriginal = function(consumers, generatedLine, generatedColumn) {
+var findOriginal = function(consumers, generatedLine, generatedColumn, shouldIgnoreMissingRanges) {
     var currentLine = generatedLine;
     var currentColumn = generatedColumn;
     var original = null;
 
     consumers.forEach(function(consumer) {
+        if (shouldIgnoreMissingRanges && currentLine === null) {
+            // We found a mapping that fails to match a statement in the previous
+            // item in the compilation chain. Prevent a fatal exception with this
+            // check.
+            // When forEach completes, findOriginal will return an object with
+            // null'd fields. The caller will then exit gracefully.
+            return;
+        }
         original = consumer.originalPositionFor({
             line: currentLine,
             column: currentColumn
@@ -17,7 +25,7 @@ var findOriginal = function(consumers, generatedLine, generatedColumn) {
     return original;
 };
 
-var createMergedSourceMap = function(maps) {
+var createMergedSourceMap = function(maps, shouldIgnoreMissingRanges) {
     var consumers = maps.map(function(map) {
         return new SourceMap.SourceMapConsumer(map);
     }).reverse();
@@ -27,7 +35,14 @@ var createMergedSourceMap = function(maps) {
     });
 
     consumers[0].eachMapping(function(mapping) {
-        var original = findOriginal(consumers, mapping.generatedLine, mapping.generatedColumn);
+        var original = findOriginal(consumers, mapping.generatedLine, mapping.generatedColumn, shouldIgnoreMissingRanges);
+        // source-map uses nulled fields to indicate that it did not find a match.
+        if (original.line === null && shouldIgnoreMissingRanges) {
+            return;
+        }
+        // Else: SourceMapGenerator will throw a nice exception for us when we call
+        // `addMapping`.
+
         generator.addMapping({
             generated: {
                 line: mapping.generatedLine,
@@ -45,7 +60,7 @@ var createMergedSourceMap = function(maps) {
     return generator.toString();
 };
 
-var createMergedSourceMapFromFiles = function(files) {
+var createMergedSourceMapFromFiles = function(files, shouldIgnoreMissingRanges) {
     var rawDataSets = files.map(function(map) {
         return fs.readFileSync(map);
     });
@@ -53,7 +68,7 @@ var createMergedSourceMapFromFiles = function(files) {
         return JSON.parse(data);
     });
 
-    return createMergedSourceMap(maps);
+    return createMergedSourceMap(maps, shouldIgnoreMissingRanges);
 };
 
 exports.createMergedSourceMap = createMergedSourceMap;
--- spec/SourceMapMerger.spec.js
+++ spec/SourceMapMerger.spec.js
@@ -6,7 +6,8 @@ describe("SourceMapMerger", function() {
     var maps = [
         "fixtures/1.map",
         "fixtures/2.map",
-        "fixtures/merged_1_2.map"
+        "fixtures/merged_1_2.map",
+        "fixtures/merged_ignore.map"
     ].map(function(filename) {
         return JSON.parse(fs.readFileSync(__dirname + "/" + filename));
     });
@@ -24,5 +25,23 @@ describe("SourceMapMerger", function() {
         ]);
         var mergedMap = JSON.parse(mergedMapRaw);
         expect(mergedMap).toEqual(maps[2]);
+    });
+
+    it("should throw an error when merging two incomplete maps", function() {
+        expect(function() {
+            SourceMapMerger.createMergedSourceMapFromFiles([
+                __dirname + "/fixtures/ignore_1.map",
+                __dirname + "/fixtures/ignore_2.map"
+            ]);
+        }).toThrow();
+    });
+
+    it("should merge two (incomplete) maps when ignoring errors", function() {
+        var mergedMapRaw = SourceMapMerger.createMergedSourceMapFromFiles([
+            __dirname + "/fixtures/ignore_1.map",
+            __dirname + "/fixtures/ignore_2.map"
+        ], true);
+        var mergedMap = JSON.parse(mergedMapRaw);
+        expect(mergedMap).toEqual(maps[3]);
     });
 });
